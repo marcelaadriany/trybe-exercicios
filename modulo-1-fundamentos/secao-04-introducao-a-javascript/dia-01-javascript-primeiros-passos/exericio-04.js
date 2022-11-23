76c14316f76282c3355cdc301f3e6fe1f66a7290
Marcela Andrade <marcela.adriany@gmail.com>
2022-11-23 19:09:37 -0300

    finalizei exercicio-04

--- modulo-1-fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/exericio-04.js
+++ modulo-1-fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/exericio-04.js
@@ -11,14 +11,28 @@ Operados lógicos
     - Se o horário estiver entre 11 e 14, insira “Hora do almoço!!!” na variável “message”.
     - Se o horário estiver entre 4 e 11, insira “Hmmm, cheiro de café recém-passado” na variável “message”.
 - Agora imprima a variável message fora das suas condições.
-
 */
 
+const currentHour = 23;
+let message = "";
 
+if (currentHour >= 22){
+    message = "Não deveríamos comer nada agora, é hora de dormir"
+}
+else if (currentHour >= 18 && currentHour < 22){
+    message = "Rango da noite, vamos jantar :D"
+}
+else if (currentHour >= 14 && currentHour < 18){
+    message = "Vamos fazer um bolo pro café da tarde?"
+}
+else if (currentHour >= 11 && currentHour < 14){
+    message = "Hora do almoço!!!"
+}
+else if (currentHour >= 4 && currentHour < 11){
+    message = "Hmmm, cheiro de café recém-passado"
+};
 
-
-
-
+console.log(message)
 
 
 /*
@@ -27,9 +41,13 @@ Operados lógicos
 - Utilizando if/else, implemente condicionais para que:
 - Se nossa variável “weekDay” for “segunda-feira”, “terça-feira”, “quarta-feira”, “quinta-feira” ou “sexta-feira”, imprima “Oba, mais um dia de aprendizado na Trybe >:D”.
 - Se for algum dia de fim de semana, imprima “FINALMENTE, descanso merecido UwU”.
-
 */
 
-let weekDay = 'quarta-feira'
+let weekDay = 'domingo'
 
-if (weekDay = 'segunda-feira' |  )
+if (weekDay === 'segunda-feira' || weekDay === 'terça-feira' || weekDay === 'quarta-feira' || weekDay === 'quinta-feira' || weekDay === 'sexta-feira') {
+    console.log('Obra, mais um dia de aprendizado na trybe >:D')
+}
+else if (weekDay === 'sábado' || weekDay === 'domingo') {
+    console.log('FINALMENTE, descanso merecido UwU')
+};
