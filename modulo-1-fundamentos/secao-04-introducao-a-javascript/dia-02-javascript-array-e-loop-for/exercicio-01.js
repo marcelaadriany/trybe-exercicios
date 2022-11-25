da6f5e15b80674fd5ded6b48221816495b7b5fc0
Marcela Andrade <marcela.adriany@gmail.com>
2022-11-25 18:51:18 -0300

    comecei exercicios de array

--- modulo-1-fundamentos/secao-04-introducao-a-javascript/dia-02-javascript-array-e-loop-for/exercicio-01.js
+++ modulo-1-fundamentos/secao-04-introducao-a-javascript/dia-02-javascript-array-e-loop-for/exercicio-01.js
@@ -1,6 +1,20 @@
 // Arrays
 
+// Obtenha o valor “Serviços” do array menu:
 let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
-let menuServices = menu[2];
+let menuServices = menu[1]; // o valor "Serviços está na posição 1 do array"
 
 console.log(menuServices);
+
+// Procure o índice do valor “Portfólio” do array menu:
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let indexOfPortfolio = ;
+
+console.log(indexOfPortfolio);
+
+// Adicione o valor “Contato” no final do array menu:
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let indexOfPortfolio = ;
+
+console.log(indexOfPortfolio);
+
