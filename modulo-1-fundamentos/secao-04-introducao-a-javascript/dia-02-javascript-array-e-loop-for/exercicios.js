6129e14cdb1b361426df053bd61efeb35f20adaa
Marcela Andrade <marcela.adriany@gmail.com>
2022-11-25 23:12:16 -0300

    fiz o exercicio 4

--- modulo-1-fundamentos/secao-04-introducao-a-javascript/dia-02-javascript-array-e-loop-for/exercicios.js
+++ modulo-1-fundamentos/secao-04-introducao-a-javascript/dia-02-javascript-array-e-loop-for/exercicios.js
@@ -19,3 +19,11 @@ for (index = 0; index < numbers.length; index +=1){
 }
 console.log(mean.toFixed(1));
 
+// 4. Com base no código que acabou de gerar, faça com que, caso o valor final seja maior que 20, imprima a mensagem: “valor maior que 20”. Caso não seja, imprima a mensagem: “valor menor ou igual a 20”;
+if (mean > 20){
+    console.log('valor maior que 20')
+}
+else{
+    console.log('valor menos ou igual a 20')
+}
+
