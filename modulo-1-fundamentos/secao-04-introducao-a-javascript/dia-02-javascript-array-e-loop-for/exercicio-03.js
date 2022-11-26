bf0e1a00b59e6ea2574cc3fca82ea08021b97463
Marcela Andrade <marcela.adriany@gmail.com>
2022-11-25 22:01:33 -0300

    fiz o exercicio 03

---
+++ modulo-1-fundamentos/secao-04-introducao-a-javascript/dia-02-javascript-array-e-loop-for/exercicio-03.js
@@ -0,0 +1,8 @@
+// For/of
+
+// Utilize o for/of para imprimir os elementos da lista names com o console.log():
+
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+for (let name of names){
+    console.log(name)
+};
