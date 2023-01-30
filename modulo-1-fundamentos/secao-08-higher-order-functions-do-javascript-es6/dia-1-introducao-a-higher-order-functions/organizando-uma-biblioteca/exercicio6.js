2e89def3bfc721fd6560cf8f59bb690cf9c85f84
Marcela Andrade <marcela.adriany@gmail.com>
2023-01-30 20:51:32 -0300

    resolve exercicio 6

--- modulo-1-fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-1-introducao-a-higher-order-functions/organizando-uma-biblioteca/exercicio6.js
+++ modulo-1-fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-1-introducao-a-higher-order-functions/organizando-uma-biblioteca/exercicio6.js
@@ -63,3 +63,14 @@ const books = [
   
 // Adicione o código do exercício aqui:
 // Faça uma função que retorne true, caso nenhuma pessoa autora tenha nascido no mesmo ano, e false, caso contrário.
+
+const expectedResult = false;
+
+const authorUnique = () => {
+  return books.every((book) =>
+    !books.some((bookSome) =>
+      (bookSome.author.birthYear === book.author.birthYear)
+      && (bookSome.author.name !== book.author.name)));
+}
+
+console.log(authorUnique());
