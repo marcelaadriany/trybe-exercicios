b98ebfb927ffbf5a59b9265b03a2c95864ea765b
Marcela Andrade <marcela.adriany@gmail.com>
2023-01-30 20:42:41 -0300

    resolve exercicio 6

--- modulo-1-fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-1-introducao-a-higher-order-functions/organizando-uma-biblioteca/exercicio5.js
+++ modulo-1-fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-1-introducao-a-higher-order-functions/organizando-uma-biblioteca/exercicio5.js
@@ -61,4 +61,14 @@ const books = [
     },
   ];
   
-  // Adicione o código do exercício aqui:
+// Adicione o código do exercício aqui:
+// Faça uma função que retorne true, se algum livro foi lançado na década de 80, e false, caso contrário.
+
+const expectedResult = true;
+
+const someBookWasReleaseOnThe80s = () => {
+  // escreva seu código aqui
+  return books.some((book) => book.releaseYear >= 1980 && book.releaseYear <= 1989)
+}
+
+console.log(someBookWasReleaseOnThe80s());
