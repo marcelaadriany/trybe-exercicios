007758d9009decfbd4f0f48b8aa82df1c1c8bc87
Marcela Andrade <marcela.adriany@gmail.com>
2023-01-31 21:03:09 -0300

    cria arquivos de database e exercicios

---
+++ modulo-1-fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-2-higher-order-functions-sort-e-map/exercicio3.js
@@ -0,0 +1,35 @@
+const books = require('./database.js');
+
+// Construa um array de objetos a partir do array de livros.
+// Cada objeto deve conter uma propriedade author, com o nome da pessoa autora do livro, e uma propriedade age, com a idade dessa pessoa quando o livro foi lançado. O array deve ser ordenado por idade, ou seja, da pessoa mais jovem para a mais velha.
+
+const expectedResult = [
+    {
+      author: 'Isaac Asimov',
+      age: 31,
+    },
+    {
+      author: 'H. P. Lovecraft',
+      age: 38,
+    },
+    {
+      author: 'Stephen King',
+      age: 39,
+    },
+    {
+      author: 'George R. R. Martin',
+      age: 43,
+    },
+    {
+      author: 'Frank Herbert',
+      age: 45,
+    },
+    {
+      author: 'J. R. R. Tolkien',
+      age: 62,
+    },
+  ];
+  
+  const nameAndAge = () => {
+    // escreva seu código aqui
+  }
