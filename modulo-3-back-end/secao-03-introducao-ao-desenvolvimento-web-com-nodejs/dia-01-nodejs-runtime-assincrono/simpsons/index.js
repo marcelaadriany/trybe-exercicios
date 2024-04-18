5456198fb8268c9fdd754267d96ea77459668e95
marcelaadriany <marcela.adriany@gmail.com>
2024-04-18 18:12:17 -0300

    finishes day 3.1

--- modulo-3-back-end/secao-03-introducao-ao-desenvolvimento-web-com-nodejs/dia-01-nodejs-runtime-assincrono/simpsons/index.js
+++ modulo-3-back-end/secao-03-introducao-ao-desenvolvimento-web-com-nodejs/dia-01-nodejs-runtime-assincrono/simpsons/index.js
@@ -1,15 +1,45 @@
 const fs = require('fs').promises;
-const path =require('path');
 
-async function main() {
+// A - cria a função que le todos os dados do arquivo
+async function readFile () {
   try {
-    const data = await fs.readFile('simpsons.json', 'utf-8');
+    const data = await fs.readFile('./simpsons.json', 'utf-8');
     const simpsons = JSON.parse(data);
-    const strings = simpsons.map(({ id, name }) => `${id} - ${name}`);
+    const strings = simpsons.map(({id, name}) => `${id} - ${name}`);
     strings.forEach((string) => console.log(string));
-  } catch (err) {
-    console.error(`Erro ao ler o arquivo: ${err.message}`);
+  } catch (error) {
+    console.error(`Erro ao ler o arquivo: ${error.message}`);
   }
 }
 
-main()
+// B- cria a função que pega os personagens por id
+async function getSimpsonById (id) {
+  try {
+    const data = await fs.readFile('./simpsons.json', 'utf-8');
+    const simpsons = JSON.parse(data);
+    const chosenSimpson = simpsons.find((simpson) => Number(simpson.id) === id );
+    if (!chosenSimpson) {
+      throw new Error('id não encontrado')
+    } return chosenSimpson;
+  } catch (error) {
+    console.error(`Erro ao ler o arquivo: ${error.message}`);
+  }
+}
+
+// C - cria uma função que altera o arquivo, retirando os personagens com id 10 e 6
+async function filterSimpsons () {
+  const data = await fs.readFile('./simpsons.json', 'utf-8');
+  const simpsons = JSON.parse(data);
+  const newArray = simpsons.filter((simpson) => simpson.id !== '6' && simpson.id !=='10');
+  return await fs.writeFile('./simpsons.json', JSON.stringify(newArray));
+}
+
+// A função main é apenas para termos um ponto de entrada centralizado para o nosso script
+async function main() {
+//  await readFile();
+//  const simpson = await getSimpsonById(11);
+//  console.log(simpson);
+
+}
+
+main();
